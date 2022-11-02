f13d6522072fabe330a1d5dae5de23f811f01fb2
Artur <artur.balsis@gmail.com>
2022-11-02 21:59:34 +0300

    modified:   "SQL queries to related tables/2.4

--- SQL queries to related tables/2.4. База данных Интернет-магазин книг.sql
+++ SQL queries to related tables/2.4. База данных Интернет-магазин книг.sql
@@ -89,3 +89,85 @@ Query result:
 | 3      | Абрамова Катя  | 2131.49   |
 | 4      | Баранов Павел  | 480.50    |
 +--------+----------------+-----------+
+
+--5. Вывести номера заказов (buy_id) и названия этапов,  на которых они в данный момент находятся. Если заказ доставлен –  информацию о нем не выводить. Информацию отсортировать по возрастанию buy_id.
+
+SELECT buy_id, name_step
+FROM  
+    step
+    JOIN buy_step USING (step_id)
+WHERE date_step_beg IS NOT NULL AND date_step_end IS NULL
+ORDER BY buy_id 
+
+Query result:
++--------+-----------------+
+| buy_id | name_step       |
++--------+-----------------+
+| 2      | Транспортировка |
+| 3      | Доставка        |
+| 4      | Оплата          |
++--------+-----------------+
+
+--6. В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город (рассматривается только этап Транспортировка). Для тех заказов, которые прошли этап транспортировки, вывести количество дней за которое заказ реально доставлен в город. А также, если заказ доставлен с опозданием, указать количество дней задержки, в противном случае вывести 0. В результат включить номер заказа (buy_id), а также вычисляемые столбцы Количество_дней и Опоздание. Информацию вывести в отсортированном по номеру заказа виде.
+
+SELECT buy_id, DATEDIFF(date_step_end, date_step_beg) AS Количество_дней, IF(DATEDIFF(date_step_end, date_step_beg) > days_delivery,DATEDIFF(date_step_end, date_step_beg)-days_delivery,0) AS Опоздание
+FROM 
+    buy_step 
+    JOIN buy USING (buy_id)
+    JOIN client USING (client_id)
+    JOIN city USING (city_id)
+WHERE date_step_end IS NOT NULL AND step_id=3
+ORDER BY buy_id
+
+Query result:
++--------+-----------------+-----------+
+| buy_id | Количество_дней | Опоздание |
++--------+-----------------+-----------+
+| 1      | 14              | 2         |
+| 3      | 4               | 0         |
++--------+-----------------+-----------+
+
+--7. Выбрать всех клиентов, которые заказывали книги Достоевского, информацию вывести в отсортированном по алфавиту виде. В решении используйте фамилию автора, а не его id.
+
+SELECT DISTINCT name_client
+    FROM client 
+    JOIN buy USING (client_id)
+    JOIN buy_book USING (buy_id)
+    JOIN book USING (book_id)
+    JOIN author USING (author_id)
+WHERE name_author LIKE 'Достоевский%'
+ORDER BY name_client
+
+Query result:
++---------------+
+| name_client   |
++---------------+
+| Абрамова Катя |
+| Баранов Павел |
+
+
+--8. Вывести жанр (или жанры), в котором было заказано больше всего экземпляров книг, указать это количество. Последний столбец назвать Количество.
+
+    SELECT name_genre, SUM(buy_book.amount) AS Количество
+    FROM genre
+        JOIN book USING (genre_id)
+        JOIN buy_book USING (book_id)
+    GROUP BY name_genre
+    HAVING SUM(buy_book.amount) = (
+        SELECT MAX(Количество)
+        FROM ( 
+            SELECT name_genre, SUM(buy_book.amount) AS Количество
+            FROM genre
+                JOIN book USING (genre_id)
+                JOIN buy_book USING (book_id)
+            GROUP BY name_genre
+            ) query_
+                                    )
+
+
+Query result:
++------------+------------+
+| name_genre | Количество |
++------------+------------+
+| Роман      | 7          |
++------------+------------+
